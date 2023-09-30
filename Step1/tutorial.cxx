8e77f91ee8f7856079c82aa09f1b380e156f6721
Paul Ladd <vlendle@gmail.com>
2023-09-30 11:30:24 -0400

    TODOs 4 through 6

--- Step1/tutorial.cxx
+++ Step1/tutorial.cxx
@@ -1,6 +1,7 @@
 // A simple program that computes the square root of a number
 #include <cmath>
-#include <cstdlib> // TODO 5: Remove this line
+// TODO 5 DONE: Remove this line:
+// #include <cstdlib> 
 #include <iostream>
 #include <string>
 
@@ -16,8 +17,8 @@ int main(int argc, char* argv[])
   }
 
   // convert input to double
-  // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
-  const double inputValue = atof(argv[1]);
+  // TODO 4 DONE?: Replace atof(argv[1]) with std::stod(argv[1])
+  const double inputValue = std::stod(argv[1]);
 
   // calculate square root
   const double outputValue = sqrt(inputValue);
